fdef83ff7810a21bf9e06757c521ecd2243b88ee
Emmanuel Yawo YOVO <96789008+DarkBrain-LP@users.noreply.github.com>
2023-02-13 21:26:37 +0100

    Succeeded scaffold

--- MiabeInspection/Program.cs
+++ MiabeInspection/Program.cs
@@ -15,6 +15,7 @@ var app = builder.Build();
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
